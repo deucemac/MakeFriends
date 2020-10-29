6064c87b1c771d1150e12f707f4a5ca63c9d96ef
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2020-10-29 01:04:59 -0400

    added production url

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -6,7 +6,7 @@ import Axios from 'axios'
 let apiUrl
 
 const apiUrls = {
-    production: '',
+    production: 'https://friendsinaction.herokuapp.com/',
     development: 'http://localhost:3000/'
 }
 
